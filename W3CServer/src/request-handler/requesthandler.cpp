6772439edaba7ad2645148c660e617049ba31e5d
thomas <thomas.johansson@melcogot.com>
2017-02-28 13:09:40 +0000

    replaced qtfication of shared pointers

--- W3CServer/src/request-handler/requesthandler.cpp
+++ W3CServer/src/request-handler/requesthandler.cpp
@@ -10,36 +10,36 @@
 #include "vissrequest.h"
 
 //factory method for requestHandler
-std::shared_ptr<RequestHandler> RequestHandler::makeRequestHandler(QString message,QWebSocket* client)
+QSharedPointer<RequestHandler> RequestHandler::makeRequestHandler(QString message,QWebSocket* client)
 {
     JSONRequestParser jsonParser(message);
     VISSRequest* parsedRequest = jsonParser.getRequest();
 
-    std::shared_ptr<RequestHandler> handler = nullptr;
+    QSharedPointer<RequestHandler> handler;
 
 
     switch(parsedRequest->getAction())
     {
         case GET:
-            handler = std::make_shared<GetHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<GetHandler>(new GetHandler(nullptr,parsedRequest,client));
             break;
         case SET:
-            handler = std::make_shared<SetHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<SetHandler>(new SetHandler(nullptr,parsedRequest,client));
             break;
         case SUBSCRIBE:
-            handler = std::make_shared<SubscribeHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<SubscribeHandler>(new SubscribeHandler(nullptr,parsedRequest,client));
             break;
         case UNSUBSCRIBE:
-            handler = std::make_shared<UnsubscribeHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<UnsubscribeHandler>(new UnsubscribeHandler(nullptr,parsedRequest,client));
             break;
         case UNSUBSCRIBEALL:
-            handler = std::make_shared<UnsubscribeAllHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<UnsubscribeAllHandler>(new UnsubscribeAllHandler(nullptr,parsedRequest,client));
             break;
         case GETVSS:
-            handler = std::make_shared<GetVSSHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<GetVSSHandler>(new GetVSSHandler(nullptr,parsedRequest,client));
             break;
         case AUTHORIZE:
-            handler = std::make_shared<AuthorizationHandler>(nullptr,parsedRequest,client);
+            handler = QSharedPointer<AuthorizationHandler>(new AuthorizationHandler(nullptr,parsedRequest,client));
             break;
         default:
             break;
--- W3CServer/src/request-handler/requesthandler.h
+++ W3CServer/src/request-handler/requesthandler.h
@@ -3,24 +3,27 @@
 
 
 #include <QObject>
-#include <memory>
 #include <QWebSocket>
 #include "vissrequest.h"
+#include <QSharedPointer>
 
 class RequestHandler : public QObject
 {
     Q_OBJECT
+
 public:
     explicit RequestHandler(QObject *parent = 0,VISSRequest* vissrequest = 0,QWebSocket* client = 0):QObject(parent),
         p_vissrequest(vissrequest),p_client(client) {}
-    static std::shared_ptr<RequestHandler> makeRequestHandler(QString message,QWebSocket* aClient);
+    static QSharedPointer<RequestHandler> makeRequestHandler(QString message,QWebSocket* aClient);
     virtual void processRequest()=0;
+
 signals:
 
 public slots:
 protected:
     VISSRequest* p_vissrequest;
     QWebSocket*  p_client;
+
 };
 
 #endif // REQUESTHANDLER_H
