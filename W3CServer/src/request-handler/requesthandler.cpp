8d0c227bd575ee754ea70ebf70aade507d251749
Pether Pettersson <pettersson.pether@gmail.com>
2017-03-06 10:03:16 +0000

    Refactored the JsonRequestParser as well as the VISSRequest.

--- W3CServer/src/request-handler/requesthandler.cpp
+++ W3CServer/src/request-handler/requesthandler.cpp
@@ -12,34 +12,33 @@
 //factory method for requestHandler
 QSharedPointer<RequestHandler> RequestHandler::makeRequestHandler(const QString& message, WebSocketWrapper* client, QSharedPointer<VSSSignalInterface> signalInterface)
 {
-    JSONRequestParser jsonParser(message);
-    VISSRequest* parsedRequest = jsonParser.getRequest();
+    JSONRequestParser jsonParser;
+    QSharedPointer<VISSRequest> parsedRequest = jsonParser.parseJson(message);
 
     QSharedPointer<RequestHandler> handler;
-
 
     switch(parsedRequest->getAction())
     {
         case GET:
-            handler = QSharedPointer<GetHandler>(new GetHandler(nullptr, signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<GetHandler>(new GetHandler(nullptr, signalInterface, parsedRequest,client));
             break;
         case SET:
-            handler = QSharedPointer<SetHandler>(new SetHandler(nullptr,signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<SetHandler>(new SetHandler(nullptr,signalInterface, parsedRequest,client));
             break;
         case SUBSCRIBE:
-            handler = QSharedPointer<SubscribeHandler>(new SubscribeHandler(nullptr,signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<SubscribeHandler>(new SubscribeHandler(nullptr,signalInterface, parsedRequest,client));
             break;
         case UNSUBSCRIBE:
-            handler = QSharedPointer<UnsubscribeHandler>(new UnsubscribeHandler(nullptr, signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<UnsubscribeHandler>(new UnsubscribeHandler(nullptr, signalInterface, parsedRequest,client));
             break;
         case UNSUBSCRIBEALL:
-            handler = QSharedPointer<UnsubscribeAllHandler>(new UnsubscribeAllHandler(nullptr,signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<UnsubscribeAllHandler>(new UnsubscribeAllHandler(nullptr,signalInterface, parsedRequest,client));
             break;
         case GETVSS:
-            handler = QSharedPointer<GetVSSHandler>(new GetVSSHandler(nullptr,signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<GetVSSHandler>(new GetVSSHandler(nullptr,signalInterface, parsedRequest,client));
             break;
         case AUTHORIZE:
-            handler = QSharedPointer<AuthorizationHandler>(new AuthorizationHandler(nullptr, signalInterface, parsedRequest,client));
+            //handler = QSharedPointer<AuthorizationHandler>(new AuthorizationHandler(nullptr, signalInterface, parsedRequest,client));
             break;
         default:
             break;
