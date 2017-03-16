8426b50bc0330aee1954172cdf6a7e967943eb39
pettersson.pether@gmail.com <pettersson.pether@gmail.com>
2017-03-16 11:45:25 +0000

    Added the actual files since git doesn't add them automatically...

---
+++ W3CQtTestClient/src/testcase.h
@@ -0,0 +1,13 @@
+#ifndef TESTCASE_H
+#define TESTCASE_H
+
+enum class TestCase
+{
+    SUBSCRIBE_UNSUBSCRIBE,
+    SUBSCRIBEALL_UNSUBSCRIBEALL,
+    AUTHORIZE_SUCCESS,
+    GET_VSS,
+    SET_GET
+};
+
+#endif // TESTCASE_H
