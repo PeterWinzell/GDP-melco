07b770274dec410e2955e5abc367ccd61881d749
pettersson.pether@gmail.com <pettersson.pether@gmail.com>
2017-04-05 09:35:28 +0000

    Added timestamp and better looking debug output. Still need to find a way to bring the source(server, client, etc) in.

--- W3CServer/src/logger.cpp
+++ W3CServer/src/logger.cpp
@@ -3,6 +3,7 @@
 #include <QMutex>
 #include <QFile>
 #include <QTextStream>
+#include <QDateTime>
 
 #include <iostream>
 
@@ -18,36 +19,43 @@ void Logger::messageHandler(QtMsgType type, const QMessageLogContext &context, c
     static QMutex mutex;
     QMutexLocker lock(&mutex);
 
-
-    QByteArray localMsg = message.toLocal8Bit();
-
     if (type == QtDebugMsg && logLevel >= 0)
     {
-        QString msg = QString("Debug: %1 (%2:%3, %4)\n").arg(localMsg.constData(), context.file, QString::number(context.line), context.function);
+        QString msg = QString("[ Server ] [ %1 ] [ Debug    ] : %2 (%3:%4, %5)\n").arg(
+                            QDateTime::currentDateTime().toString("yyyy-MM-dd : hh:mm:ss.zzz"),
+                            message, context.file, QString::number(context.line),context.function);
         std::cerr << msg.toStdString().c_str();
         if(logToFile) { writeLogToFile(msg); }
     }
     else if (type == QtInfoMsg && logLevel >= 1)
     {
-        QString msg = QString("Info: %s (%s:%u, %s)\n").arg(localMsg.constData(), context.file, QString::number(context.line), context.function);
+        QString msg = QString("[ Server ] [ %1 ] [ Info     ] : %2 (%3:%4, %5)\n").arg(
+                           QDateTime::currentDateTime().toString("yyyy-MM-dd : hh:mm:ss.zzz"),
+                           message, context.file, QString::number(context.line), context.function);
         std::cerr << msg.toStdString().c_str();
         if(logToFile) { writeLogToFile(msg); }
     }
     else if (type == QtWarningMsg && logLevel >= 2)
     {
-        QString msg = QString("Warning: %s (%s:%u, %s)\n").arg(localMsg.constData(), context.file, QString::number(context.line), context.function);
+        QString msg = QString("[ Server ] [ %1 ] [ Warning  ] : %2 (%3:%4, %5)\n").arg(
+                           QDateTime::currentDateTime().toString("yyyy-MM-dd : hh:mm:ss.zzz"),
+                           message, context.file, QString::number(context.line), context.function);
         std::cerr << msg.toStdString().c_str();
         if(logToFile) { writeLogToFile(msg); }
     }
     else if (type == QtCriticalMsg && logLevel >= 3)
     {
-        QString msg = QString("Critical: %s (%s:%u, %s)\n").arg(localMsg.constData(), context.file, QString::number(context.line), context.function);
+        QString msg = QString("[ Server ] [ %1 ] [ Critical ] : %2 (%3:%4, %5)\n").arg(
+                            QDateTime::currentDateTime().toString("yyyy-MM-dd : hh:mm:ss.zzz"),
+                            message, context.file, QString::number(context.line), context.function);
         std::cerr << msg.toStdString().c_str();
         if(logToFile) { writeLogToFile(msg); }
     }
     else if (type == QtFatalMsg && logLevel >= 4)
     {
-        QString msg = QString("Fatal: %s (%s:%u, %s)\n").arg(localMsg.constData(), context.file, QString::number(context.line), context.function);
+        QString msg = QString("[ Server ] [ %1 ] [ Fatal    ] : %2 (%3:%4, %5)\n").arg(
+                            QDateTime::currentDateTime().toString("yyyy-MM-dd : hh:mm:ss.zzz"),
+                            message, context.file, QString::number(context.line), context.function);
         std::cerr << msg.toStdString().c_str();
 
         if(logToFile) { writeLogToFile(msg); }
