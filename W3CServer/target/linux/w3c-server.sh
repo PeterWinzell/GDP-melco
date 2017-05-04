90b5aaba964cbf955407cfd24f5f831b7a9dbfa7
Thomas Johansson <thomas.johansson@melcogot.com>
2017-05-04 11:03:03 +0200

    Changed default config to suit demo rig

--- W3CServer/target/linux/w3c-server.sh
+++ W3CServer/target/linux/w3c-server.sh
@@ -2,18 +2,17 @@
 # Linux service controller script
 
 service=W3CServer
-params="-secure"
 
 case "$1" in
     start)
-        start-stop-daemon --start --quiet --background --exec /usr/bin/$service -- $params
+        start-stop-daemon --start --quiet --background --exec /usr/bin/$service
         ;;
     stop)
         killall $service
         ;;
     restart)
         killall $service
-        start-stop-daemon --start --quiet --background --exec /usr/bin/$service -- $params
+        start-stop-daemon --start --quiet --background --exec /usr/bin/$service
         ;;
     *)
         echo "Usage: $0 {start|stop|restart}"
